b6b3162881859962f148e42938f85e2d61190597
Pablo Ribalta Lorenzo <pablo.ribaltalorenzo@gmail.com>
2015-12-28 14:59:59 +0100

    Removed filter files and added cosmetic fixes

--- s2/s2t09/s2t09.cpp
+++ s2/s2t09/s2t09.cpp
@@ -12,7 +12,9 @@ template<typename T>
 class ThreadSafeStack
 {
 public:
-  ThreadSafeStack() {}
+  ThreadSafeStack()
+  {
+  }
 
   ThreadSafeStack(const ThreadSafeStack& other)
   {
@@ -69,3 +71,4 @@ int main()
 {
   return 0;
 }
+
